8aab85a384360c44edc5ceff7fcdbd98b0d1595c
ashu290996 <ashu290996@gmail.com>
2025-09-04 11:40:59 +0530

    updated security group configuration

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "8081,8082,8083"
+    destination_port_range     = ["8081", "8082", "8083"]
     source_address_prefix      = "*"
     destination_address_prefix = "*"
     
