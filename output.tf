38c694b11241f91346a3c102e1872e7252971378
ashu290996 <ashu290996@gmail.com>
2025-10-11 19:12:21 +0530

    updated output.tf file

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "resource_group_name" {
 output "public_ip_address" {
   value = azurerm_linux_virtual_machine.my_terraform_vm[*].public_ip_address
 }
+
+output "virtual_machine_name" {
+  value = azurerm_linux_virtual_machine.my_terraform_vm[*].name
+  }
