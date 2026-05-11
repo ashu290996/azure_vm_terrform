2bddcc6b2b3f67c75fd1ad3d9a12362310b19d90
ashu290996 <ashu290996@gmail.com>
2026-05-12 00:34:13 +0530

    added new user_node pool and qa env

---
+++ user_nodepool.tf
@@ -0,0 +1,40 @@
+resource "azurerm_kubernetes_cluster_node_pool" "user" {
+  name                  = var.user_node_pool_name1
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size               = var.user_node_pool_vm_size
+  node_count            = var.user_node_pool_node_count
+  min_count             = var.user_node_pool_min_count
+  max_count             = var.user_node_pool_max_count
+  auto_scaling_enabled  = true
+  os_disk_size_gb       = var.user_node_pool_os_disk_size_gb
+  os_disk_type          = "Managed"
+  max_pods              = var.user_node_pool_max_pods
+  mode                  = "User"
+  vnet_subnet_id        = azurerm_subnet.aks.id
+  zones                 = var.user_node_pool_availability_zones
+
+  # Node Labels
+  node_labels = merge(
+    var.user_node_pool_node_labels,
+    {
+      "nodepool-type" = "user"
+      "environment"   = var.environment
+    }
+  )
+
+  # Node Taints (optional)
+  node_taints = var.user_node_pool_node_taints
+
+  # Upgrade Settings
+  upgrade_settings {
+    max_surge = "33%"
+  }
+
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = [
+      node_count
+    ]
+  }
+}
